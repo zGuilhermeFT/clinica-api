309bbc15d45251f26c39b2cfd3c70806506abd64
zGuilhermeFT <guilhermemineft@gmail.com>
2021-07-26 22:31:22 -0300

    fix heroku error

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 module.exports = {
     dialect: 'postgres',
-    host: process.env.DB_HOST,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    host: 'tuffi.db.elephantsql.com',
+    username: 'psycchnm',
+    password: 'UbVT6PwV-jxEJgFuPn-7BNwI8j2YmQnc',
+    database: 'psycchnm',
     define: {
         timestamps: true,
         underscored: true
